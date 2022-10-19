aac65f1980f6724ca9c78a33d72e75060d85b806
codd1e <andreinik32@gmail.com>
2022-10-19 20:41:37 +0300

    changed link to repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer () {
             <div className="footer-copyright">
                 <div className="container">
                     © {new Date().getFullYear()} Copyright Text
-                    <a className="grey-text text-lighten-4 right" href="https://github.com/codd1e/react-movies">Repo</a>
+                    <a className="grey-text text-lighten-4 right" href="https://github.com/codd1e/react-shop">Repo</a>
                 </div>
             </div>
         </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header () {
             <div className="nav-wrapper">
                 <a href="#" className="brand-logo">Movies</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#">Repo</a></li>
+                    <li><a href="https://github.com/codd1e/react-shop">Repo</a></li>
                 </ul>
             </div>
         </nav>
